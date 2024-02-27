ede12495e7fcd579000d85159396e70e87b98200
BlackOligarch <godfred.hammond2018@yahoo.com>
2024-02-27 17:35:52 +0000

    Added 2.Repetition Token #1

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,4 +1,10 @@
 #!/usr/bin/env ruby
-# This script matches lines with 1 or more letter 't'
 
-puts ARGV[0].scan(/hbt+n/).join
+input_string = ARGV[0]
+
+if input_string.nil?
+  puts "Usage: ./2-repetition_token_1.rb <input_string>"
+  exit 1
+end
+
+puts input_string.scan(/hbt+n/).join
