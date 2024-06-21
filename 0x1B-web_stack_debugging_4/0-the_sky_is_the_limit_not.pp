7a79113a4e1cc5ea920b34f3ae2e4961e3732c21
BlackOligarch <godfred.hammond2018@yahoo.com>
2024-06-21 14:32:42 +0000

    done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+exec { 'nginx-limit-fix':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# restart the service entirely while
+# ensuring the above patch is applied
+exec { 'nginx-limit-fix':
+  command => 'nginx restart',
+  path    => '/etc/init.d/',
+  require => Exec['fix-for-nginx'],
+}
